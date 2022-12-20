33f346ce40110741c7e90c31386d3bef70204087
info-mic-info <jobsocial94@gmail.com>
2022-12-20 14:39:35 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+
+
+
+
+// Descrizione: Rifare l'esercizio della to do list. Ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            todolist: [
+                {
+                    text: "Comprare latte",
+                    done: true
+                },
+                {
+                    text: "Comprare uova",
+                    done: true
+                },
+                {
+                    text: "Comprare farina",
+                    done: true
+                },
+                {
+                    text: "Comprare lievito",
+                    done: true
+                },
+                {
+                    text: "Comprare zucchero",
+                    done: true
+                }
+            ]
+
+        }
+    },
+
+    methods: {
+        // MILESTONE 1
+        // Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+        if()
+
+    }
+}).mount('#app')
